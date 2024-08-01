0352d05b04d342c0856ec6960896cbc47e1a7e72
chianti-ga <chiantigally@gmail.com>
2024-08-01 15:23:25 +0200

    make shared library wasm compliant for use in the backend

--- shared/src/user.rs
+++ shared/src/user.rs
@@ -1,15 +1,27 @@
+#[cfg(target_arch = "x86_64")]
 use oauth2::basic::BasicTokenResponse;
 use serde::{Deserialize, Serialize};
 
 use crate::discord::User;
 use crate::fiche_rp::FicheRP;
 
+#[cfg(target_arch = "x86_64")]
 #[derive(Serialize, Deserialize)]
 pub struct Account {
     pub discord_user: User,
     pub discord_roles: Vec<String>,
     pub token: BasicTokenResponse,
     pub last_renewal: u64,
+    pub fiches: Vec<FicheRP>,
+    pub creation_date: u64,
+    pub banned: bool,
+}
+
+#[cfg(target_arch = "wasm32")]
+#[derive(Serialize, Deserialize)]
+pub struct Account {
+    pub discord_user: User,
+    pub discord_roles: Vec<String>,
     pub fiches: Vec<FicheRP>,
     pub creation_date: u64,
     pub banned: bool,
