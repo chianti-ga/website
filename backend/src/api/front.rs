3b60a2c040d2831d51d985c1b212b580782114dd
chianti-ga <chiantigally@gmail.com>
2024-08-02 20:56:29 +0200

    switch to query for wasm convenience

--- backend/src/api/front.rs
+++ backend/src/api/front.rs
@@ -2,27 +2,29 @@ use actix_session::Session;
 use actix_web::{get, HttpRequest, HttpResponse, Responder, web};
 use mongodb::bson::doc;
 use mongodb::Collection;
+use serde::Deserialize;
 use serenity::futures::TryStreamExt;
 
 use shared::user::FrontAccount;
-
 use crate::AppData;
 use crate::utils::auth_utils::is_auth_valid;
 
+#[derive(Deserialize, Clone)]
+struct Auth {
+    pub auth_id: String,
+}
 #[get("/api/front/retrieve_accounts")]
-pub async fn retrieve_accounts(req: HttpRequest, session: Session, app_data: web::Data<AppData>) -> impl Responder {
-    if let Some(cookie) = req.cookie("auth_id") {
-        if is_auth_valid(cookie.clone().value(), app_data.dbclient.clone()).await {
-            let accounts: Collection<FrontAccount> = app_data.dbclient.database("visualis-website").collection("account");
+pub async fn retrieve_accounts(auth: web::Query<Auth>, session: Session, app_data: web::Data<AppData>) -> impl Responder {
+    return if is_auth_valid(&*auth.auth_id, app_data.dbclient.clone()).await {
+        let accounts: Collection<FrontAccount> = app_data.dbclient.database("visualis-website").collection("account");
 
-            let query = doc! {
-                "auth_id" : cookie.value()
-            };
-            let vec_front_account: Vec<FrontAccount> = accounts.find(query).await.expect("Can't retrieve accounts").try_collect().await.expect("Can't set account into vec");
-            return HttpResponse::Ok().json(&vec_front_account);
-        }
+        let query = doc! {
+            "auth_id" : &auth.auth_id
+        };
+        let vec_front_account: Vec<FrontAccount> = accounts.find(query).await.expect("Can't retrieve accounts").try_collect().await.expect("Can't set account into vec");
+
+        HttpResponse::Ok().json(&vec_front_account)
     } else {
-        return HttpResponse::BadRequest().body("");
+        HttpResponse::Unauthorized().body("")
     }
-    return HttpResponse::BadRequest().body("");
 }
