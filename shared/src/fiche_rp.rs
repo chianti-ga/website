8a63f5e922fa2472276388fb7a255f0f81ecf31b
chianti-ga <chiantigally@gmail.com>
2024-08-05 17:26:56 +0200

    make level enum iterable

--- shared/src/fiche_rp.rs
+++ shared/src/fiche_rp.rs
@@ -1,7 +1,7 @@
 use std::fmt::{Display, Formatter};
 
 use serde::{Deserialize, Serialize};
-
+use strum::EnumIter;
 use crate::user::FrontAccount;
 
 #[derive(Serialize, Deserialize, Clone)]
@@ -47,7 +47,7 @@ pub struct ReviewMessage {
     pub set_state: FicheState,
 }
 
-#[derive(Serialize, Deserialize, Clone)]
+#[derive(Serialize, Deserialize, Clone, PartialEq, Eq)]
 pub enum FicheState {
     Waiting,
     RequestModification,
@@ -70,7 +70,7 @@ impl FicheState {
 }
 
 /**     JOB INFO STARTS HERE    **/
-#[derive(Serialize, Deserialize, Clone)]
+#[derive(Serialize, Deserialize, Clone, PartialEq, Eq)]
 pub enum Job {
     Security(SecurityRole),
     Science(ScienceRole),
@@ -78,6 +78,48 @@ pub enum Job {
     Doctor,
     SiteDirector,
     Chaos,
+}
+impl Job {
+    pub fn get_science_role(&self) -> Option<&ScienceRole> {
+        return match self {
+            Job::Security(_) => None,
+            Job::Science(role) => Option::from(role),
+            Job::ClassD => None,
+            Job::Doctor => None,
+            Job::SiteDirector => None,
+            Job::Chaos => None,
+        }
+    }
+    pub fn get_security_role(&self) -> Option<&SecurityRole> {
+        return match self {
+            Job::Security(role) => Option::from(role),
+            Job::Science(_) => None,
+            Job::ClassD => None,
+            Job::Doctor => None,
+            Job::SiteDirector => None,
+            Job::Chaos => None,
+        }
+    }
+    pub fn get_science_level(&self) -> Option<&ScienceLevel> {
+        return match self {
+            Job::Security(_) => None,
+            Job::Science(role) => Option::from(role.get_science_level()),
+            Job::ClassD => None,
+            Job::Doctor => None,
+            Job::SiteDirector => None,
+            Job::Chaos => None,
+        }
+    }
+    pub fn get_security_level(&self) -> Option<&SecurityLevel> {
+        return match self {
+            Job::Security(role) => Option::from(role.get_security_level()),
+            Job::Science(_) => None,
+            Job::ClassD => None,
+            Job::Doctor => None,
+            Job::SiteDirector => None,
+            Job::Chaos => None,
+        }
+    }
 }
 impl Display for Job {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
@@ -88,43 +130,63 @@ impl Display for Job {
             Job::Doctor => write!(f, "Médecin"),
             Job::SiteDirector => write!(f, "Directeur du Site"),
             Job::Chaos => write!(f, "Chaos"),
+
         }
     }
 }
-#[derive(Serialize, Deserialize, Clone)]
+#[derive(Serialize, Deserialize, Clone, Eq, PartialEq)]
 pub enum ScienceRole {
-    Assistant(ScienceLevel),
+    Scientific(ScienceLevel),
     Researcher(ScienceLevel),
     Supervisor(ScienceLevel),
+}
+impl ScienceRole {
+    fn get_science_level(&self) -> &ScienceLevel {
+        return match self {
+            ScienceRole::Scientific(level) => level,
+            ScienceRole::Researcher(level) => level,
+            ScienceRole::Supervisor(level) => level
+        }
+    }
 }
 impl Display for ScienceRole {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match self {
-            ScienceRole::Assistant(level) => write!(f, "Assitant {}", level),
+            ScienceRole::Scientific(level) => write!(f, "Scientifique {}", level),
             ScienceRole::Researcher(level) => write!(f, "Chercheur {}", level),
             ScienceRole::Supervisor(level) => write!(f, "Superviseur {}", level),
         }
     }
 }
-#[derive(Serialize, Deserialize, Clone)]
+#[derive(Serialize, Deserialize, Clone, Eq, PartialEq)]
 pub enum ScienceLevel {
     Beginner,
     Confirmed,
     Senior,
 }
+
 impl Display for ScienceLevel {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match self {
-            ScienceLevel::Beginner => write!(f, "Débutant"),
+            ScienceLevel::Beginner => write!(f, "Junior"),
             ScienceLevel::Confirmed => write!(f, "Confirmé"),
-            ScienceLevel::Senior => write!(f, "Sénior")
+            ScienceLevel::Senior => write!(f, "Sénior"),
         }
     }
 }
-#[derive(Serialize, Deserialize, Clone)]
+#[derive(Serialize, Deserialize, Clone, Eq, PartialEq)]
 pub enum SecurityRole {
     SecurityOfficier(SecurityLevel),
     TacticalAgent(SecurityLevel),
+}
+
+impl SecurityRole {
+    fn get_security_level(&self) -> &SecurityLevel {
+        return match self {
+            SecurityRole::SecurityOfficier(level) => level,
+            SecurityRole::TacticalAgent(level) => level,
+        }
+    }
 }
 impl Display for SecurityRole {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
@@ -134,7 +196,7 @@ impl Display for SecurityRole {
         }
     }
 }
-#[derive(Serialize, Deserialize, Clone)]
+#[derive(Serialize, Deserialize, Clone, Eq, PartialEq, EnumIter)]
 pub enum SecurityLevel {
     Rct,
     Sdt,
@@ -177,7 +239,7 @@ impl Display for SecurityLevel {
             SecurityLevel::Cmd => write!(f, "Commandant"),
             SecurityLevel::LtCol => write!(f, "Lieutenant-Colonel"),
             SecurityLevel::Col => write!(f, "Colonel"),
-            SecurityLevel::Gen => write!(f, "Général")
+            SecurityLevel::Gen => write!(f, "Général"),
         }
     }
 }
